cdab5c793dc0ef72eddef4f789d90773b1925664
leerivera <leerivera80@gmail.com>
2022-04-12 19:11:41 -0400

    netflix

--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,20 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class NetFlixShow{
+    constructor(name, episodes, duration, seasons){
+        this.name = name
+        this.episodes = episodes
+        this.duration = duration
+        this.seasons = seasons
 
+    }
+    watch(){
+        alert("youre watching")
+    }
+    whatSeason(){
+        alert("on season 8")
+    }
+    howLong(){
+        alert("very long time")
+    }
+}
